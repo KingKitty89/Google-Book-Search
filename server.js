6bace81dd1d13d6594895d2199a67c2a15559abf
Jessica Helft <jessicahelft@gmail.com>
2020-07-28 19:23:44 -0500

    database working with seed

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const path = require("path");
+const routes = require('./routes');
+const mongoose = require('mongoose');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -12,12 +15,24 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Define API routes here
+app.use(routes);
 
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
+// Connect to the Mongo DB
+mongoose
+    .connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(console.log('Database is connected'))
+    .catch(err => console.log(err));
+
+
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
